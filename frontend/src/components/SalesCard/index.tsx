448eeb84f0f5b7f3dd03cd95bce1bde4f55a0e71
eduardomsj <eduardomsj@gmail.com>
2022-07-15 19:58:21 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -11,7 +11,7 @@ function SalesCard() {
 
     const [minDate, setMinDate] = useState(min)
     const [maxDate, setMaxDate] = useState(max)
-
+    
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
